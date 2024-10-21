e23aaedee5d624240fcdb72f30cc13ff3cb9897b
Tingmao Wang <tingmaowang@microsoft.com>
2024-10-21 15:31:41 +0100

    More helpful check_http output

--- scripts/vm_helpers/check_http.ps1
+++ scripts/vm_helpers/check_http.ps1
@@ -3,18 +3,19 @@ try {
   Set-Alias -Name shimdiag -Value C:\ContainerPlat\shimdiag.exe
 
   (crictl pods -o json | convertfrom-json).items | foreach {
-    $podId=$_.id; $podName=$_.metadata.name;
-    $ip=(crictl inspectp $podId | ConvertFrom-Json).info.cniResult.Interfaces.eth0.IPConfigs.IP;
-    echo "Checking $podName - $ip";
+    $podId=$_.id
+    $podName=$_.metadata.name
+    $ip=(crictl inspectp $podId | ConvertFrom-Json).info.cniResult.Interfaces.eth0.IPConfigs.IP
+    echo "Checking $podName - $ip"
     try {
-      $res=Invoke-RestMethod -TimeoutSec 1 -Uri "http://${ip}:8000/index.txt";
+      $res=Invoke-RestMethod -TimeoutSec 1 -Uri "http://${ip}:8000/index.txt"
       if ($res.Trim() -ne "Hello") {
-        Write-Output 'ERROR: unexpected response:' $res;
+        Write-Output "ERROR: unexpected response: $res"
       }
     } catch {
-      Write-Output 'ERROR: failed to check HTTP' $_.Exception.ToString()
+      Write-Output "ERROR: failed to check HTTP on pod $podName" $_.Exception.ToString()
     }
   }
 } catch {
-  Write-Output 'ERROR: failed to run check_http' $_.Exception.ToString()
+  Write-Output "ERROR: failed to run check_http on pod $podName" $_.Exception.ToString()
 }
